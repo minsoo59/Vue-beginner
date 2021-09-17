cc498dd72ec3c69464315ff5f44db9a628fba2fa
shushu <minsoo59@naver.com>
2021-09-17 15:32:43 +0900

    8. 지뢰찾기

--- 8.지뢰찾기/mineSwiper.vue
+++ 8.지뢰찾기/mineSwiper.vue
@@ -9,16 +9,29 @@
 
 <script>
 import { mapState } from "vuex";
-import store from "./store";
+import store, { INCREMENT_TIMER } from "./store";
 import TableComponent from "./TableComponent";
 import MineForm from "./MineForm";
+
+let interval;
 export default {
-  components: { TableComponent, MineForm },
   store,
+  components: { TableComponent, MineForm },
   computed: {
-    ...mapState(["timer", "result"]),
+    ...mapState(["timer", "result", "halted"]),
   },
   methods: {},
+  watch: {
+    halted(value, oldValue) {
+      if (value === false) {
+        interval = setInterval(() => {
+          this.$store.commit(INCREMENT_TIMER);
+        }, 1000);
+      } else {
+        clearInterval(interval);
+      }
+    },
+  },
 };
 </script>
 
--- 8.지뢰찾기/store.js
+++ 8.지뢰찾기/store.js
@@ -65,6 +65,7 @@ export default new Vuex.Store({
       mine: 0,
     },
     timer: 0,
+    halted: true, // 중단된 상태
     result: "",
   }, // vue의 data와 비슷
   getters: {}, // vue의 computed와 비슷
@@ -80,13 +81,112 @@ export default new Vuex.Store({
 
       state.tableData = plantMine(row, cell, mine);
       state.timer = 0;
+      state.halted = false;
     },
-    [OPEN_CELL](state) {},
-    [CLICK_MINE](state) {},
-    [FLAG_CELL](state) {},
-    [QUESTION_CELL](state) {},
-    [NORMALIZE_CELL](state) {},
-    [INCREMENT_TIMER](state) {},
+    [OPEN_CELL](state, { row, cell }) {
+      const checked = [];
+      function checkAround() {
+        const checkRowOrCellIsUnderfined =
+          row < 0 ||
+          row >= state.tableData.length ||
+          cell < 0 ||
+          cell >= state.tableData[0].length;
+        if (checkRowOrCellIsUnderfined) {
+          return;
+        }
+        if (
+          [
+            CODE.OPENED,
+            CODE.FLAG,
+            CODE.FLAG_MINE,
+            CODE.QUESTION_MINE,
+            CODE.QUESTION,
+          ].includes(state.tableData[row][cell])
+        ) {
+          return;
+        }
+        if (checked.includes(row + "/" + cell)) {
+          return;
+        } else {
+          checked.push(row + "/" + cell);
+        }
+        let around = [];
+        if (state.tableData[row - 1]) {
+          around = around.concat([
+            state.tableData[row - 1][cell - 1],
+            state.tableData[row - 1][cell],
+            state.tableData[row - 1][cell + 1],
+          ]);
+        }
+        around = around.concat([
+          state.tableData[row][cell - 1],
+          state.tableData[row][cell + 1],
+        ]);
+        if (state.tableData[row + 1]) {
+          around = around.concat([
+            state.tableData[row + 1][cell - 1],
+            state.tableData[row + 1][cell],
+            state.tableData[row + 1][cell + 1],
+          ]);
+        }
+        const counted = around.filter(function(v) {
+          return [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v);
+        });
+        if (counted.length == 0 && row > -1) {
+          const near = [];
+          if (row - 1 > -1) {
+            near.push([row - 1, cell - 1]);
+            near.push([row - 1, cell]);
+            near.push([row - 1, cell + 1]);
+          }
+          near.push([row, cell - 1]);
+          near.push([row, cell + 1]);
+          if (row + 1 < state.tableData.length) {
+            near.push([row + 1, cell - 1]);
+            near.push([row + 1, cell]);
+            near.push([row + 1, cell + 1]);
+          }
+          near.forEach((n) => {
+            if (state.tableData[n[0]][n[1]] !== CODE.OPENED) {
+              checkAround(n[0], n[1]);
+            }
+          });
+        }
+        if (state.tableData[row][cell] === CODE.NORMAL) {
+          openedCount += 1;
+        }
+        Vue.set(state.tableData[row], cell, counted.length);
+      }
+      checkAround(row, cell);
+    },
+    [CLICK_MINE](state, { row, cell }) {
+      state.halted = true;
+      Vue.set(state.tableData[row], cell, CODE.CLICKED_MINE);
+    },
+    [FLAG_CELL](state, { row, cell }) {
+      if (state.tableData[row][cell] === CODE.MINE) {
+        Vue.set(state.tableData[row], cell, CODE.FLAG_MINE);
+      } else {
+        Vue.set(state.tableData[row], cell, CODE.FLAG);
+      }
+    },
+    [QUESTION_CELL](state, { row, cell }) {
+      if (state.tableData[row][cell] === CODE.FLAG_MINE) {
+        Vue.set(state.tableData[row], cell, CODE.QUESTION_MINE);
+      } else {
+        Vue.set(state.tableData[row], cell, CODE.QUESTION);
+      }
+    },
+    [NORMALIZE_CELL](state, { row, cell }) {
+      if (state.tableData[row][cell] === CODE.QUESTION_MINE) {
+        Vue.set(state.tableData[row], cell, CODE.MINE);
+      } else {
+        Vue.set(state.tableData[row], cell, CODE.NORMAL);
+      }
+    },
+    [INCREMENT_TIMER](state) {
+      state.timer += 1;
+    },
   }, // state를 수정할 때 사용, 동기적으로
   actions: {}, // 비동기를 사용할때, 또는 여러 mutation을 연달아 실행할때
 });
